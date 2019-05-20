c99a14e64daffd0d645473a8c3a44a7c1b41c260
mjmj <yuemjmahu@gmail.com>
2019-05-20 10:42:35 +0900

    2019.05.20)ERMaster 최신화 완료

--- KOSMO_Proj/src/main/webapp/SQL(DB)/newfile.sql
+++ KOSMO_Proj/src/main/webapp/SQL(DB)/newfile.sql
@@ -1,283 +1,53 @@
+
 /* Drop Tables */
 
-
-DROP TABLE baby_border CASCADE CONSTRAINTS;
-DROP TABLE calender  CASCADE CONSTRAINTS;
-DROP TABLE reservation CASCADE CONSTRAINTS;
-DROP TABLE carpool_border  CASCADE CONSTRAINTS;
+DROP TABLE babypair CASCADE CONSTRAINTS;
+DROP TABLE Baby_Border CASCADE CONSTRAINTS;
+DROP TABLE Calender CASCADE CONSTRAINTS;
+DROP TABLE Reservation CASCADE CONSTRAINTS;
+DROP TABLE Carpool_Border CASCADE CONSTRAINTS;
 DROP TABLE hospital CASCADE CONSTRAINTS;
-DROP TABLE kizcafe CASCADE CONSTRAINTS;
-DROP TABLE review CASCADE CONSTRAINTS;
-DROP TABLE partner_members CASCADE CONSTRAINTS;
-DROP TABLE members CASCADE CONSTRAINTS;
-DROP TABLE VISIT CASCADE CONSTRAINTS;
+DROP TABLE ReView CASCADE CONSTRAINTS;
+DROP TABLE KidHouse CASCADE CONSTRAINTS;
+DROP TABLE Partner_Members CASCADE CONSTRAINTS;
+DROP TABLE Members CASCADE CONSTRAINTS;
+DROP TABLE visit CASCADE CONSTRAINTS;
 
-DROP SEQUENCE SEQ_Baby_Border;
-DROP SEQUENCE SEQ_Calender;
-DROP SEQUENCE SEQ_Carpool_Border;
-DROP SEQUENCE SEQ_Hospital;
-DROP SEQUENCE SEQ_KidHouse;
-DROP SEQUENCE SEQ_Map;
-DROP SEQUENCE SEQ_Partner_Members;
-DROP SEQUENCE SEQ_Reservation;
-DROP SEQUENCE SEQ_ReView;
-DROP SEQUENCE SQE_Emergency;
-DROP TRIGGER  TRG_RESER;
+/* Drop Sequence */
 
-CREATE SEQUENCE SEQ_Baby_Border
+DROP SEQUENCE SEQ_Babypair;
+DROP SEQUENCE SEQ_calender;
+DROP SEQUENCE SEQ_carpool_border;
+DROP SEQUENCE SEQ_partner_members;
+DROP SEQUENCE SEQ_reservation;
+
+/* Drop Trigger */
+DROP TRIGGER TRG_RESER;
+
+/* Create Sequence */
+
+CREATE SEQUENCE SEQ_babypair
+NOCYCLE
+NOCACHE;
+CREATE SEQUENCE SEQ_calender
+NOCYCLE
+NOCACHE;
+CREATE SEQUENCE SEQ_carpool_border
+NOCYCLE
+NOCACHE;
+CREATE SEQUENCE SEQ_partner_members
+NOCYCLE
+NOCACHE;
+CREATE SEQUENCE SEQ_reservation
 NOCYCLE
 NOCACHE;
 
-CREATE SEQUENCE SEQ_Calender
-NOCYCLE
-NOCACHE;
-
-CREATE SEQUENCE SEQ_Carpool_Border
-NOCYCLE
-NOCACHE;
-
-CREATE SEQUENCE SEQ_Hospital
-NOCYCLE
-NOCACHE;
-
-CREATE SEQUENCE SEQ_KidHouse
-NOCYCLE
-NOCACHE;
-
-CREATE SEQUENCE SEQ_Map
-NOCYCLE
-NOCACHE;
-
-CREATE SEQUENCE SEQ_Partner_Members
-NOCYCLE
-NOCACHE;
-
-CREATE SEQUENCE SEQ_Reservation
-NOCYCLE
-NOCACHE;
-
-CREATE SEQUENCE SEQ_ReView
-NOCYCLE
-NOCACHE;
-
-CREATE SEQUENCE SQE_Emergency
-NOCYCLE
-NOCACHE;
-
-
-
-
-
-/* Create Tables */
-
-CREATE TABLE baby_border
-(
-	no number NOT NULL,
-	title nvarchar2(100) NOT NULL,
-	thumbnail varchar2(1000),
-	url varchar2(1000) NOT NULL,
-	PRIMARY KEY (no)
-);
-
-
-CREATE TABLE calender 
-(
-	cal_no number NOT NULL,
-	title nvarchar2(2000) NOT NULL,
-	startdate nvarchar2(25) NOT NULL,
-	enddate nvarchar2(25),
-	content CLOB,
-	type nvarchar2(20),
-	id varchar2(20) NOT NULL,
-	PRIMARY KEY (Cal_no)
-);
-
-
-CREATE TABLE carpool_border 
-(
-	cp_no number NOT NULL,
-	startpoint nvarchar2(500) NOT NULL,
-	endpoint nvarchar2(500) NOT NULL,
-	type nvarchar2(10),
-	content CLOB,
-	start_Xpos number NOT NULL,
-	start_Ypos number NOT NULL,
-	end_Xpos number NOT NULL,
-	end_Ypos number NOT NULL,
-	id varchar2(20) NOT NULL,
-	-- 0 : 카시트 미 존재
-	-- 1 : 카시트 존재
-	carseat char(1) DEFAULT '0' CONSTRAINTS carseat_CK CHECK (carseat = '0' OR carseat = '1'),
-	time nvarchar2(50) NOT NULL,
-	price number NOT NULL,
-	-- 0 : 현재 예약되지 않음
-	-- 1 : 예약중
-	status char(1) DEFAULT '0' CONSTRAINTS status_CK CHECK (status = '0' OR status = '1'),
-	PRIMARY KEY (cp_no)
-);
-
-
-CREATE TABLE hospital(
-	h_no number primary key,
-	name nvarchar2(100),
-	tel nvarchar2(30),
-	addr nvarchar2(1000),
-	-- 월요일부터 공휴일까지 입니다.
-	mon nvarchar2(100),
-	tue nvarchar2(100),
-	wed nvarchar2(100),
-	thu nvarchar2(100),
-	fri nvarchar2(100),
-	sat nvarchar2(100),
-	sun nvarchar2(100),
-	holiday nvarchar2(100),
-	emergency number,
-	-- 0 이면 일반 병원
-	-- 1 이면 치과
-	-- 2 이면 산부인과
-	code number
-);
-
-
-CREATE TABLE kizcafe
-(
-	k_no number NOT NULL,
-	name nvarchar2(100),
-	tel nvarchar2(30),
-	addr nvarchar2(1000),
-	PRIMARY KEY (k_no)
-);
-
-CREATE TABLE review
-(
-	rv_no number NOT NULL,
-	title nvarchar2(50),
-	content nvarchar2(2000),
-	score number,
-	id varchar2(20) NOT NULL,
-	h_no number,
-	k_no number,
-	PRIMARY KEY (rv_no)
-);
-
-CREATE TABLE members
-(
-	id varchar2(20) NOT NULL,
-	-- 최소 : 6자
-	-- 최대 : 16자
-	-- 영문과 숫자와 특수문자의 조합으로...
-	pwd varchar2(16) NOT NULL,
-	name nvarchar2(5) NOT NULL,
-	addr nvarchar2(100) NOT NULL,
-	email varchar2(50) NOT NULL,
-	tel nvarchar2(15) NOT NULL,
-	--photo varchar2(300) NOT NULL,
-	-- 이메일 인증 키 입니다.
-	authkey nvarchar2(50),
-	-- 이메일 인증이 완료가되면 값이 들어갑니다. 
-	-- 0이면 인증이 되지 않았습니다. 
-	-- 1이면 인증이 되어있는 상태입니다.
-    -- 추후 시큐리티와 밀접한 연관이 있기때문에 체크하지않으면 아예 홈페이지가 맛이갑니다.
-	enabled number(1) DEFAULT 0 CONSTRAINTS enabled_CK CHECK (enabled = 0 OR enabled = 1),
-    partnerstatus char(1) DEFAULT '0' CONSTRAINTS partnerstatus_CK CHECK (partnerstatus = '0' OR partnerstatus = '1'),
-	AUTHORITY nvarchar2(20)  DEFAULT 'ROLE_USER',
-	PRIMARY KEY (id)
-);
-
-
-CREATE TABLE partner_members
-(
-	p_no number NOT NULL,
-	id varchar2(20) NOT NULL,
-	carnumber nvarchar2(20),
-	-- 소형차
-	-- 중형차
-	-- 대형차
-	cartype nvarchar2(10),
-	career nvarchar2(10),
-	totalscore number,
-	totalcount number,
-	PRIMARY KEY (p_no)
-);
-
-
-CREATE TABLE reservation
-(
-	rs_no number NOT NULL,
-	score number,
-	-- 계약이 완료되었는지 판단하는 컬럼입니다
-	-- 0이면 아직 진행중, 1이면 완료되어 완료테이블에서 조회 가능합니다.
-	finish char(1) DEFAULT '0' CONSTRAINTS finish_CK CHECK (finish = '0' OR finish = '1'),
-	id varchar2(20) NOT NULL,
-	cp_no number NOT NULL,
-	PRIMARY KEY (rs_no)
-);
-
-CREATE TABLE VISIT (V_DATE DATE);
-
-
-
-/* Create Foreign Keys */
-
-ALTER TABLE reservation
-	ADD FOREIGN KEY (cp_no)
-	REFERENCES carpool_border (cp_no)
-    on delete cascade
-;
-
-ALTER TABLE review
-	ADD FOREIGN KEY (h_no)
-	REFERENCES hospital (h_no)
-    on delete cascade
-;
-
-ALTER TABLE review
-	ADD FOREIGN KEY (k_no)
-	REFERENCES kizcafe (k_no)
-    on delete cascade
-;
-
-
-ALTER TABLE calender 
-	ADD FOREIGN KEY (id)
-	REFERENCES members (id)
-    on delete cascade
-;
-
-
-ALTER TABLE carpool_border 
-	ADD FOREIGN KEY (id)
-	REFERENCES members (id)
-    on delete cascade
-;
-
-
-ALTER TABLE partner_members
-	ADD FOREIGN KEY (id)
-	REFERENCES members (id)
-    on delete cascade
-;
-
-
-ALTER TABLE reservation
-	ADD FOREIGN KEY (id)
-	REFERENCES members (id)
-    on delete cascade
-;
-
-
-ALTER TABLE review
-	ADD FOREIGN KEY (id)
-	REFERENCES members (id)
-    on delete cascade
-;
-
--- 트리거
+/* Create Trigger */
 
 CREATE TRIGGER TRG_RESER
 AFTER INSERT OR DELETE
 ON reservation
-FOR EACH ROW --행단위 트리거
+FOR EACH ROW
 DECLARE
 BEGIN
 	IF INSERTING THEN
@@ -288,53 +58,225 @@ BEGIN
 		WHERE cp_no = :OLD.cp_no;
 	END IF;
 END;
-/
 
 
--- 관리자 아이디넣기
-insert into members values('admin','asd123!@#','관리자','주소','이메일','01012345678','인증키','1',1,default);
+/* Create Tables */
 
--- 카풀 게시판
-
-insert into members values('lee','1234','이길동','천호동','adf@nate.com','01012345678',0,1,0,default);
-insert into members values('kim','1234','김길동','천호동','adf@nate.com','01012345678',0,1,0,default);
-insert into members values('park','1234','박길동','천호동','adf@nate.com','01012345678',0,1,0,default);
-insert into members values('choi','1234','최길동','천호동','adf@nate.com','01012345678',0,1,0,default);
-
-insert into carpool_border values(10,'강남역','역삼역','태워주세요','연락처 010-1234-5678',37.498184,127.028484,37.500474,127.036082,'lee',0,sysdate,10000,'0');
-insert into carpool_border values(11,'강남역','역삼역','태워주세요','연락처 010-1234-5678',37.498184,127.028484,37.500474,127.036082,'park',0,sysdate,10000,'0');
-insert into carpool_border values(12,'강남역','역삼역','태워주세요','연락처 010-1234-5678', 37.498184,127.028484,37.500474,127.036082,'choi',0,sysdate,10000,'0');
-
---insert into reservation values(1,0,'0','lee',10);
---insert into reservation values(2,0,'0','park',11);
---insert into reservation values(3,0,'0','choi',12);
-
-insert into baby_border(no,title,url,thumbnail) values(1,'설명 아직 미정','https://www.youtube.com/watch?v=gcAbeTWjHvQ','http://img.youtube.com/vi/gcAbeTWjHvQ/mqdefault.jpg');
-insert into baby_border(no,title,url,thumbnail) values(2,'설명 아직 미정','https://www.youtube.com/watch?v=pe1asLzVQBQ','http://img.youtube.com/vi/pe1asLzVQBQ/mqdefault.jpg');
-insert into baby_border(no,title,url,thumbnail) values(3,'설명 아직 미정','https://www.youtube.com/watch?v=IDPPkozCxOw','http://img.youtube.com/vi/IDPPkozCxOw/mqdefault.jpg');
-insert into baby_border(no,title,url,thumbnail) values(4,'설명 아직 미정','https://www.youtube.com/watch?v=2ktbTawmR28','http://img.youtube.com/vi/2ktbTawmR28/mqdefault.jpg');
-insert into baby_border(no,title,url,thumbnail) values(5,'설명 아직 미정','https://www.youtube.com/watch?v=krbcaUy8K24','http://img.youtube.com/vi/krbcaUy8K24/mqdefault.jpg');
-insert into baby_border(no,title,url,thumbnail) values(6,'설명 아직 미정','https://www.youtube.com/watch?v=Z0usnVLGWtM','http://img.youtube.com/vi/Z0usnVLGWtM/mqdefault.jpg');
-insert into baby_border(no,title,url,thumbnail) values(7,'설명 아직 미정','https://www.youtube.com/watch?v=FqFmMFrRWew','http://img.youtube.com/vi/FqFmMFrRWew/mqdefault.jpg');
-insert into baby_border(no,title,url,thumbnail) values(8,'설명 아직 미정','https://www.youtube.com/watch?v=M4x-SWwc6dI','http://img.youtube.com/vi/M4x-SWwc6dI/mqdefault.jpg');
-insert into baby_border(no,title,url,thumbnail) values(9,'설명 아직 미정','https://www.youtube.com/watch?v=ZnLQKTXAtMs','http://img.youtube.com/vi/ZnLQKTXAtMs/mqdefault.jpg');
-
-select * from calender;
-desc calender;
-
-INSERT INTO calender VALUES(SEQ_Calender.NEXTVAL,'All Day Event','2019-05-01','','내용','행사','kim');
-INSERT INTO calender VALUES(SEQ_Calender.NEXTVAL,'Long Event','2019-05-07','2019-05-10','내용','중요','lee');
-INSERT INTO calender VALUES(SEQ_Calender.NEXTVAL,'Conference','2019-05-06','2019-05-08','내용','타입','admin');
-INSERT INTO calender VALUES(SEQ_Calender.NEXTVAL,'Meeting','2019-05-07T10:30:00','2019-05-07T12:30:00','내용','기념일','kim');
-INSERT INTO calender VALUES(SEQ_Calender.NEXTVAL,'Lunch','2019-06-20T12:00:00','','내용','약속','lee');
-INSERT INTO calender VALUES(SEQ_Calender.NEXTVAL,'Birthday Party','2019-05-08T07:00:00','2019-05-08T20:00:00','내용','타입','lee');
-INSERT INTO calender VALUES(SEQ_Calender.NEXTVAL,'Meeting2','2019-07-07T14:30:00','','내용','타입','admin');
-INSERT INTO calender VALUES(SEQ_Calender.NEXTVAL,'Happy Hour','2019-08-07T17:30:00','','내용','타입','lee');
-INSERT INTO calender VALUES(SEQ_Calender.NEXTVAL,'Dinner','2019-05-07T20:00:00','','내용','타입','admin');
+CREATE TABLE babypair
+(
+	baby_no number NOT NULL,
+	title nvarchar2(50) NOT NULL,
+	period nvarchar2(100) NOT NULL,
+	time nvarchar2(100) NOT NULL,
+	place nvarchar2(2000) NOT NULL,
+	titleimage nvarchar2(2000) NOT NULL,
+	product nvarchar2(2000),
+	Cal_No number NOT NULL,
+	PRIMARY KEY (baby_no)
+);
 
 
-alter trigger TRG_RESER enable;
+CREATE TABLE Baby_Border
+(
+	No number NOT NULL,
+	Title nvarchar2(20) NOT NULL,
+	Thumbnail varchar2(300),
+	Url varchar2(300) NOT NULL,
+	PRIMARY KEY (No)
+);
 
-commit;
 
-purge recyclebin;
+CREATE TABLE Calender
+(
+	Cal_No number NOT NULL,
+	Title nvarchar2(2000) NOT NULL,
+	StartDate nvarchar2(100) NOT NULL,
+	EndDate nvarchar2(100),
+	Content clob,
+	Type nvarchar2(20),
+	ID varchar2(20) NOT NULL,
+	PRIMARY KEY (Cal_No)
+);
+
+
+CREATE TABLE Carpool_Border
+(
+	CP_No number NOT NULL,
+	StartPoint nvarchar2(20) NOT NULL,
+	EndPoint nvarchar2(20) NOT NULL,
+	Type nvarchar2(10),
+	Content nvarchar2(20),
+	Start_Xpos number NOT NULL,
+	Start_Ypos number NOT NULL,
+	End_Xpos number NOT NULL,
+	End_Ypos number NOT NULL,
+	ID varchar2(20) NOT NULL,
+	carseat nvarchar2(10),
+	-- 출발 시간을 적습니다
+	TIME date NOT NULL,
+	-- 카풀 금액입니다.
+	PRICE number NOT NULL,
+	-- 0 : 예약안됨
+	-- 1 :  예약중
+	status char(1),
+	opened varchar2(100),
+	signed varchar2(100),
+	done varchar2(100),
+	reviewed varchar2(100),
+	PRIMARY KEY (CP_No)
+);
+
+
+CREATE TABLE hospital
+(
+	h_no number NOT NULL,
+	name nvarchar2(100),
+	tel nvarchar2(30),
+	addr nvarchar2(1000),
+	mon nvarchar2(100),
+	tue nvarchar2(100),
+	wed nvarchar2(100),
+	thu nvarchar2(100),
+	sat nvarchar2(100),
+	fri nvarchar2(100),
+	sun nvarchar2(100),
+	holiday nvarchar2(100),
+	emergency number,
+	code number,
+	PRIMARY KEY (h_no)
+);
+
+
+CREATE TABLE KidHouse
+(
+	k_no number NOT NULL,
+	name nvarchar2(100),
+	tel nvarchar2(30),
+	addr nvarchar2(1000),
+	PRIMARY KEY (k_no)
+);
+
+
+CREATE TABLE Members
+(
+	ID varchar2(20) NOT NULL,
+	-- 최소 : 6자
+	-- 최대 : 16자
+	-- 영문과 숫자와 특수문자의 조합으로...
+	PWD varchar2(16) NOT NULL,
+	Addr nvarchar2(100) NOT NULL,
+	Email varchar2(50) NOT NULL,
+	Tel nvarchar2(50) NOT NULL,
+	photo nvarchar2(500),
+	-- 이메일 인증용 랜덤키 입니다.
+	authkey nvarchar2(50),
+	enabled number,
+	-- 0또는 1
+	partnerstatus char(1) DEFAULT '0',
+	authority nvarchar2(20) DEFAULT 'ROLE_USER',
+	PRIMARY KEY (ID)
+);
+
+
+CREATE TABLE Partner_Members
+(
+	P_No number NOT NULL,
+	ID varchar2(20) NOT NULL,
+	CarNumber nvarchar2(20),
+	-- 소형차
+	-- 중형차
+	-- 대형차
+	CarType nvarchar2(10),
+	career nvarchar2(20),
+	TotalScore number,
+	TotalCount number,
+	PRIMARY KEY (P_No)
+);
+
+
+CREATE TABLE Reservation
+(
+	RS_No number NOT NULL,
+	Score number,
+	-- 리뷰 제목
+	title nvarchar2(500),
+	content nvarchar2(2000),
+	Finish char(1) DEFAULT '1' NOT NULL,
+	ID varchar2(20) NOT NULL,
+	CP_No number NOT NULL,
+	PRIMARY KEY (RS_No)
+);
+
+
+CREATE TABLE ReView
+(
+	RV_No number NOT NULL,
+	Title nvarchar2(50),
+	Content nvarchar2(2000),
+	Score number,
+	ID varchar2(20) NOT NULL,
+	k_no number NOT NULL,
+	PRIMARY KEY (RV_No)
+);
+
+
+CREATE TABLE visit
+(
+	V_DATE date NOT NULL,
+	PRIMARY KEY (V_DATE)
+);
+
+
+
+/* Create Foreign Keys */
+
+ALTER TABLE babypair
+	ADD FOREIGN KEY (Cal_No)
+	REFERENCES Calender (Cal_No)
+;
+
+
+ALTER TABLE Reservation
+	ADD FOREIGN KEY (CP_No)
+	REFERENCES Carpool_Border (CP_No)
+;
+
+
+ALTER TABLE ReView
+	ADD FOREIGN KEY (k_no)
+	REFERENCES KidHouse (k_no)
+;
+
+
+ALTER TABLE Calender
+	ADD FOREIGN KEY (ID)
+	REFERENCES Members (ID)
+;
+
+
+ALTER TABLE Carpool_Border
+	ADD FOREIGN KEY (ID)
+	REFERENCES Members (ID)
+;
+
+
+ALTER TABLE Partner_Members
+	ADD FOREIGN KEY (ID)
+	REFERENCES Members (ID)
+;
+
+
+ALTER TABLE Reservation
+	ADD FOREIGN KEY (ID)
+	REFERENCES Members (ID)
+;
+
+
+ALTER TABLE ReView
+	ADD FOREIGN KEY (ID)
+	REFERENCES Members (ID)
+;
+
+
+ALTER TRIGGER TRG_RESER ENABLE;
+
